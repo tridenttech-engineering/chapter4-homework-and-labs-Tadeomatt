c35472fc157c5a2daecf5685691213a10753d93f
Tadeomatt <tadeomatt4@gmail.com>
2025-02-12 01:19:54 +0000

    Submission for lab4-4

--- .ccls-cache/@home@runner@workspace/lab4-4.cpp
+++ .ccls-cache/@home@runner@workspace/lab4-4.cpp
@@ -1,16 +1,22 @@
 //Lab4-4.cpp - displays the volume of a cylinder
 //Created/revised by <your name> on <current date>
-
-{
-} //end of main function
-cin >> height;
-cout << "Height: ";
-cout << "Radius: ";
-cout << "Volume: " << volume << endl;
-double height   = 0.0;
-double radius   = 0.0;
-double volume   = 0.0;
-int main()
-return 0;
+#include<iostream>
 using namespace std;
-volume = PI * radius * radius * height;
+int main()
+{
+  //declare variables and constants
+  double height = 0.0;
+  double radius = 0.0;
+  double volume = 0.0;
+  const double PI = 3.14;
+  //input data
+  cout << "Height: ";
+  cin >> height;
+  cout << "Radius: ";
+  cin >> radius;
+  //calculate volume of a cylinder
+  volume = PI * radius * radius * height;
+  //display volume
+  cout << "Volume: " << volume << endl;
+  return 0;
+} //end of main function
